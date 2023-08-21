75302d40ec130f89dcac942ecd82bd9c30692944
AzwadFawadHasan2020222 <azwadfawadhasan@gmail.com>
2023-08-21 10:58:53 +0600

    imported home_screen to main

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 void main(){
   runApp(const MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "calculator App",
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
